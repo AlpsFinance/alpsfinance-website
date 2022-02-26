4e887c00a138f328bd6ac51c4dad7d0b36f51aae
alb_rt <albert.jonathan23@gmail.com>
2022-02-27 05:28:00 +0800

    Remove fusionchart delayed code

--- component/Tokenomics.tsx
+++ component/Tokenomics.tsx
@@ -1,10 +1,8 @@
 import { FC } from "react";
-import dynamic from "next/dynamic.js";
-const FC = dynamic(() => import("./next-fusioncharts.js"), { ssr: false });
 
 const Tokenomics: FC = () => {
   // Create a JSON object to store the chart configurations
-  return <FC></FC>;
+  return <div />
 };
 
 export default Tokenomics;
