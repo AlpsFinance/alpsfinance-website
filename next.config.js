3071429b2314be3b5e29677bbfd3fad7daf1771a
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-04-24 03:57:04 +0700

    add trailingHash to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
   ...withImages(),
   images: {
     loader: "akamai",
