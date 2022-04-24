0abdcc3c58d32d0cf3bd51c674a9597492655763
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-04-24 22:47:26 +0700

    adding AlpsFeatures component

--- component/AlpsFeatures.tsx
+++ component/AlpsFeatures.tsx
@@ -8,12 +8,23 @@ import Image from "next/image";
 const featuresList = [
   {
     title: "Cross-Chain, Cross-Platform",
-    description: "Cross Chain Features",
+    description: `
+      As we are converging to a cross-chain world, our platform will 
+      be offering multiple blockchain support by default. Furthermore, 
+      we shall be making the platform accessible to all device,
+      mainly on web and mobile.
+    `,
     image: "/features/multichain.png",
   },
   {
     title: "Gamified Social Media",
-    description: "Social media",
+    description: `
+      We believe in the power of community, which is why a core
+      component of our platform is an in-app gamified social media that
+      enables users interaction. You are not alone as you will be able
+      to connect, follow, engage, learn and compete with
+      other users investing on our platform.
+    `,
     image: "/features/community.png",
   },
 ];
@@ -27,7 +38,7 @@ const AlpsFeatures: FC = () => {
       container
       direction="column"
       py={3}
-      px={isLargeScreen ? 5 : 3}
+      px={isLargeScreen ? 10 : 3}
       alignItems="center"
       justifyContent="center"
       spacing={3}
@@ -38,7 +49,7 @@ const AlpsFeatures: FC = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <Grid container spacing={3} direction="column">
+        <Grid container spacing={5} direction="column">
           {featuresList.map((feature, index) => {
             const { title, description, image } = feature;
             return (
@@ -46,24 +57,38 @@ const AlpsFeatures: FC = () => {
                 <Grid
                   container
                   direction={index % 2 === 0 ? "row-reverse" : "row"}
+                  spacing={5}
+                  mb={5}
                 >
-                  <Grid item xs={12} lg={6}>
-                    <Grid container direction="column">
+                  <Grid item xs={12} md={6}>
+                    <Grid
+                      container
+                      direction="column"
+                      justifyContent="center"
+                      spacing={1}
+                      sx={isLargeScreen ? { height: "100%" } : {}}
+                    >
                       <Grid item>
                         <Typography variant={isLargeScreen ? "h5" : "h6"}>
                           <b>{title}</b>
                         </Typography>
                       </Grid>
-                      <Grid item>{description}</Grid>
+                      <Grid item>
+                        <Typography variant="subtitle1">
+                          {description}
+                        </Typography>
+                      </Grid>
                     </Grid>
                   </Grid>
-                  <Grid item xs={12} lg={6}>
-                    <Image
-                      src={image}
-                      alt={title}
-                      width={isLargeScreen ? 800 : 350}
-                      height={isLargeScreen ? 600 : 200}
-                    />
+                  <Grid item xs={12} md={6}>
+                    <Grid container justifyContent="center" alignItems="center">
+                      <Image
+                        src={image}
+                        alt={title}
+                        width={isLargeScreen ? 550 : 350}
+                        height={isLargeScreen ? 400 : 270}
+                      />
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
