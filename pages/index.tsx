7a3a498d9d7d66629242567770c6b9f80e627620
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-04-28 21:41:18 +0700

    finsihing polishing the header

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,7 @@ import Fade from "@mui/material/Fade";
 import Slide from "@mui/material/Slide";
 import Button from "@mui/material/Button";
 import AlpsFeatures from "../component/AlpsFeatures";
+import ReactTypingEffect from "react-typing-effect";
 
 const Home: NextPage = () => {
   const theme = useTheme();
@@ -47,19 +48,16 @@ const Home: NextPage = () => {
           mt={isLargeScreen ? "80px" : 0}
           spacing={isLargeScreen ? 0 : 2}
         >
-          <Grid item xs={12} md={4}>
+          <Grid item xs={12} md={5} lg={4}>
             <Grid
               container
               direction="column"
-              justifyContent="center"
-              alignItems="center"
               spacing={2}
               sx={
                 isLargeScreen
                   ? {}
                   : {
                       pt: 10,
-                      px: 3,
                     }
               }
             >
@@ -77,41 +75,123 @@ const Home: NextPage = () => {
                   unmountOnExit
                   timeout={1000}
                 >
-                  <Grid container alignItems="center" justifyContent="center">
+                  <Grid
+                    container
+                    alignItems="center"
+                    justifyContent={isLargeScreen ? "start" : "center"}
+                  >
                     <Typography
-                      variant={isLargeScreen ? "h3" : "h5"}
-                      align="center"
+                      variant={isLargeScreen ? "h4" : "h5"}
+                      align={isLargeScreen ? "left" : "center"}
                     >
                       <b>
-                        Social <br />
-                        + <br />
-                        DeFi
+                        Expand your Network.
+                        <br /> Grow your Net Worth.
                       </b>
                     </Typography>
                   </Grid>
                 </Slide>
-              </Grid>
-              {/* <Grid
-                item
-                sx={{
-                  padding: "10px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
-                  align={isLargeScreen ? "left" : "center"}
-                  variant={isLargeScreen ? "h6" : "body1"}
-                  mx={0}
+                <Grid
+                  container
+                  mt={1}
+                  alignItems="center"
+                  justifyContent={isLargeScreen ? "start" : "center"}
                 >
-                  Alps is the simplest way to build and manage your entire DeFi
-                  + NFT portfolio across multiple chains from one dashboard.
-                  Explore the world of decentralized finance today.
-                </Typography>
-              </Grid> */}
+                  <Grid item>
+                    <Typography
+                      variant={isLargeScreen ? "h6" : "subtitle1"}
+                      sx={{ fontFamily: "'Quicksand', sans-serif" }}
+                      align={isLargeScreen ? "left" : "center"}
+                    >
+                      Be a 21st Century Investor. <br />
+                      Build your{" "}
+                      <b>
+                        <ReactTypingEffect
+                          text={[
+                            "Cross-Chain DeFi strategy.",
+                            "Cross-Chain Farming strategy.",
+                            "NFT Metaverse DAO.",
+                            "Hedge Fund DAO.",
+                          ]}
+                          speed={100}
+                        />
+                      </b>
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} mt={2}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    fullWidth
+                    sx={{
+                      borderRadius: 3,
+                      py: 1,
+                      px: 5,
+                      fontWeight: "bold",
+                      background: "white",
+                    }}
+                    onClick={() => {
+                      const AlpsFinanceAppURL = "https://app.alps.finance";
+                      window.open(AlpsFinanceAppURL, "_blank") ||
+                        window.location.replace(AlpsFinanceAppURL);
+                    }}
+                  >
+                    <Typography>
+                      <b
+                        style={{
+                          background:
+                            "linear-gradient(to right, #597DFD, #B091F9)",
+                          WebkitBackgroundClip: "text",
+                          WebkitTextFillColor: "transparent",
+                        }}
+                      >
+                        Launch App
+                      </b>
+                    </Typography>
+                  </Button>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    fullWidth
+                    sx={{
+                      borderRadius: 3,
+                      py: 1,
+                      px: 5,
+                      fontWeight: "bold",
+                      background: "white",
+                    }}
+                    onClick={() => {
+                      const AlpsFinanceAppURL =
+                        "https://storageapi.fleek.co/yosephks-team-bucket/AlpsFinanceWhitepaper.pdf";
+                      window.open(AlpsFinanceAppURL, "_blank") ||
+                        window.location.replace(AlpsFinanceAppURL);
+                    }}
+                  >
+                    <Typography>
+                      <b
+                        style={{
+                          background:
+                            "linear-gradient(to right, #597DFD, #B091F9)",
+                          WebkitBackgroundClip: "text",
+                          WebkitTextFillColor: "transparent",
+                        }}
+                      >
+                        Whitepaper
+                      </b>
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={12} md={8}>
+          <Grid item xs={12} md={7} lg={8}>
             <Grid container justifyContent="flex-end">
               <Fade in timeout={1000}>
                 <Grid item>
@@ -125,39 +205,6 @@ const Home: NextPage = () => {
               </Fade>
             </Grid>
           </Grid>
-          {!isLargeScreen && (
-            <Grid item xs={12}>
-              <Button
-                color="primary"
-                variant="contained"
-                fullWidth
-                sx={{
-                  borderRadius: 3,
-                  py: 1,
-                  px: 5,
-                  fontWeight: "bold",
-                  background: "white",
-                }}
-                onClick={() => {
-                  const AlpsFinanceAppURL = "https://app.alps.finance";
-                  window.open(AlpsFinanceAppURL, "_blank") ||
-                    window.location.replace(AlpsFinanceAppURL);
-                }}
-              >
-                <Typography>
-                  <b
-                    style={{
-                      background: "linear-gradient(to right, #597DFD, #B091F9)",
-                      WebkitBackgroundClip: "text",
-                      WebkitTextFillColor: "transparent",
-                    }}
-                  >
-                    Launch App
-                  </b>
-                </Typography>
-              </Button>
-            </Grid>
-          )}
         </Grid>
       </Grid>
       <WhyAlpsFinance />
