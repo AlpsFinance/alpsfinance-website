e5492e7b3c0380df7136a77e69f59ca47738da04
alb_rt <albert.jonathan23@gmail.com>
2022-05-04 23:51:09 +0700

    make it env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ export default function MyApp(props: MyAppProps) {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PM2VXFC');
+          })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}');
         `}
       </Script>
       <ThemeProvider theme={theme}>
